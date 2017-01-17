c0ea400d94728d48128744cafdb26f1388ed341a
f0dsterz <Fod@junaids-macbook-air.local>
2017-01-17 21:32:42 +1300

    Initial Commit

---
+++ TwitterTrendsClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterTrendsClient
+//
+//  Created by f0dsterz on 17/01/17.
+//  Copyright © 2017 frankszs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
